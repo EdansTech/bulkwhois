a8927374a7105e136058033548f1e6286f97516b
Edan <37985949+EdansTech@users.noreply.github.com>
2021-03-26 13:56:24 +0000

    Removed space after \n

--- run.rb
+++ run.rb
@@ -7,17 +7,16 @@ requestlimit=3 # Number of requests you want to make before the program dies (se
 domainlist=File.open('TEXT FILE WITH DOMAINS.txt').read # One domain per line
 cooldown=5 # Cooldown between requests in seconds
 
-
 line_num=0
 requestlimitif=requestlimit-1
 domainlist.gsub!(/\r\n?/, "\n")
 domainlist.each_line do |line|
     if "#{line_num}" > "#{requestlimitif}"
-        abort("\n Your request limit of #{requestlimit} has been reached")
+        abort("\nYour request limit of #{requestlimit} has been reached")
         exit(0)
     end
 
-    print "Currently on domain number #{line_num += 1} (#{line})"
+    print "\nCurrently on domain number #{line_num += 1} / #{line}"
     apiurl = ("https://api.ip2whois.com/v1?key=#{apikey}&domain=#{line}")
     uri = URI(apiurl)
     response = Net::HTTP.get(uri)
@@ -26,30 +25,30 @@ domainlist.each_line do |line|
     # Fatal error handling
     results = JSON.parse(response, object_class: OpenStruct)
     if results.error_code == 101
-        abort("\n API key not found.")
+        abort("\nAPI key not found.")
         exit(0)
     end
     if results.error_code == 102
-        abort("\n API key disabled.")
+        abort("\nAPI key disabled.")
         exit(0)
     end
     if results.error_code == 103
-        abort("\n API key expired.")
+        abort("\nAPI key expired.")
         exit(0)
     end
     if results.error_code == 104
-        abort("\n Insufficient credits.")
+        abort("\nInsufficient credits.")
         exit(0)
     end
     if results.error_code == 105
-        abort("\n Unknown error.")
+        abort("\nUnknown error.")
         exit(0)
     end
 
     # Here you can do whatever you want with the output from the api for example 
     # if you just wanted to print the response it would be something like:
     # print "#{output}"
-    # however in my case I want it to create a new text document containing 
+    # however in my case i want it to create a new text document containing 
     # the json result for every domain
 
     File.open("#{line}.log", "w") { |f| f.write "#{output}" }
